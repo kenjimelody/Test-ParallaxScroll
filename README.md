8f5c62faca6b4bfcf1a0383ee4797ee608620149
Thanisak Kenji <kenji.melody@gmail.com>
2016-11-08 14:17:20 +0700

    Initial Code

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test Parallax with CoordinatorLayout
---
+++ app/src/main/java/com/superhardcode/www/test_parallaxscroll/MainActivity.java
@@ -0,0 +1,14 @@
+package com.superhardcode.www.test_parallaxscroll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
